d6e71c0d61c1a8bab049d9209dfa7477cb1d9ecb
Hiep Phuc Thai <itmanagertph@gmail.com>
2025-07-06 12:56:15 +0000

    Modify downloader.sh to download AOD 2021 dataset
    Signed-off-by: Hiep Phuc Thai <itmanagertph@gmail.com>

--- dataset/downloader.sh
+++ dataset/downloader.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 # MPAir.csv
-if [ ! -f "MPAir.csv" ]; then
+if [ ! -f "MPair.csv" ]; then
     gdown 1lklbnCBydhcs7y1wu9wK3o5aTXC3_EGO
 fi
 
-# Matching data 2022
+# AOD data 2022
 if [ ! -f "MatchingData2022.xlsx" ]; then
     gdown 1aKzff4vq3VULWpQJzVk3Ibl4B0MEDwFw
+fi
+
+# AOD data 2021
+if [ ! -f "aod_data_daily.csv" ]; then
+    gdown 1wncw0PQVhB2xwJoAR2wsUbmQi0k4yIRp
 fi
