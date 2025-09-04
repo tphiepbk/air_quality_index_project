7f4956a0b6a88e886aadc17bd7c35eadf5a3146e
Hiep Phuc Thai <itmanagertph@gmail.com>
2025-09-04 07:19:35 +0000

    Add more estimators to MICE method
    Signed-off-by: Hiep Phuc Thai <itmanagertph@gmail.com>

--- src/data_utils.py
+++ src/data_utils.py
@@ -1,10 +1,23 @@
 from sklearn.experimental import enable_iterative_imputer
 from sklearn.impute import IterativeImputer
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.linear_model import BayesianRidge
 
-def mice(df):
+def mice(df, method=None):
+    if method is not None:
+        assert method in ["random_forest", "extra_trees"]
     df_columns = df.columns
-    imputer = IterativeImputer(random_state=100, max_iter=100, keep_empty_features=True)
+    if method == "random_forest":
+        estimator = RandomForestRegressor()
+        max_iter = 10
+    elif method == "extra_trees":
+        estimator = ExtraTreesRegressor()
+        max_iter = 10
+    else:
+        estimator = BayesianRidge()
+        max_iter = 100
+    imputer = IterativeImputer(estimator=estimator, random_state=100, max_iter=max_iter, keep_empty_features=True)
     imputer.fit(df[df_columns])
     imputed_value = imputer.transform(df[df_columns])
     mice_df = df.copy()
