79452d3f442485d168e9a655a62949d7de6557c8
Hiep Phuc Thai <itmanagertph@gmail.com>
2025-09-11 07:06:32 +0000

    Working with API to predict pm25 using AOD data
    Signed-off-by: Hiep Phuc Thai <itmanagertph@gmail.com>

--- src/loop_model.py
+++ src/loop_model.py
@@ -5,11 +5,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from src.plot import plot_prediction
-from src.config_reader import ConfigurationReader
 
-conf = ConfigurationReader("/le_thanh_van_118/workspace/hiep_workspace/air_quality_index_project/model_params.json").data
-
-def generate_loopresults(range_of_dimension, reduction_model_class, prediction_model_class, X_scaled: pd.DataFrame, y_scaled: pd.DataFrame, label_scaler, with_pm25_3km=True):
+def generate_loopresults(X_scaled: pd.DataFrame, y_scaled: pd.DataFrame,
+                         label_scaler,
+                         range_of_dimension,
+                         reduction_model_class,
+                         prediction_model_class,
+                         reduction_n_past=7, reduction_n_future=7,
+                         reduction_epochs=50, reduction_batch_size=128,
+                         prediction_n_past=7, prediction_n_future=1,
+                         prediction_epochs=50, prediction_batch_size=128,
+                         with_pm25_3km=False):
     # Get supported metrics
     supported_metrics = prediction_model_class.get_supported_metrics()
     
@@ -30,8 +36,8 @@ def generate_loopresults(range_of_dimension, reduction_model_class, prediction_m
         reduction = reduction_model_class(X_scaled,
                                         val_percentage=0.2, test_percentage=0.2,
                                         latent_dim=n,
-                                        n_past=conf["reduction"]["n_past"], n_future=conf["reduction"]["n_future"],
-                                        epochs=conf["reduction"]["epochs"], batch_size=conf["reduction"]["batch_size"],
+                                        n_past=reduction_n_past, n_future=reduction_n_future,
+                                        epochs=reduction_epochs, batch_size=reduction_batch_size,
                                         verbose=0,
                                         model_name=reduction_model_name)
         X_encoded, encoder_model_path = reduction.execute()
@@ -43,8 +49,8 @@ def generate_loopresults(range_of_dimension, reduction_model_class, prediction_m
         prediction = prediction_model_class(X_encoded, y_scaled,
                                             label_scaler,
                                             val_percentage=0.2, test_percentage=0.2,
-                                            n_past=conf["prediction"]["n_past"], n_future=conf["prediction"]["n_future"],
-                                            epochs=conf["prediction"]["epochs"], batch_size=conf["prediction"]["batch_size"],
+                                            n_past=prediction_n_past, n_future=prediction_n_future,
+                                            epochs=prediction_epochs, batch_size=prediction_batch_size,
                                             model_name=prediction_model_name,
                                             verbose=0)
         prediction.get_model_info()
@@ -59,7 +65,7 @@ def generate_loopresults(range_of_dimension, reduction_model_class, prediction_m
 
     return loopresults
 
-def choose_the_best(loopresults, metric_to_choose="mae"):
+def choose_the_best(loopresults, metric_to_choose="mae", prediction_n_future=1):
     print("Choosing the best result\n")
     print(loopresults.keys())
     
@@ -90,6 +96,6 @@ def choose_the_best(loopresults, metric_to_choose="mae"):
             best_metric_value = loopresult["metrics"][metric_to_choose]
 
     # Visualize the prediction
-    plot_prediction(best_evaluation_data[0], best_evaluation_data[1], conf["prediction"]["n_future"])
+    plot_prediction(best_evaluation_data[0], best_evaluation_data[1], prediction_n_future)
 
     return best_metrics, best_num_of_components, best_encoded_data, best_evaluation_data, best_encoder_model_path
